d843360ca8eb8b678c9521e8a76269e189681b5e
grasmanek94 <github@gz0.nl>
2024-03-17 18:40:04 +0100

    Remove legacy app

--- legacy/1.0/java/eu/mrogalski/saidit/SaidIt.java
+++
@@ -1,7 +0,0 @@
-package com.grasmanek94.saidit;
-
-public class SaidIt {
-
-    static final String PACKAGE_NAME = "com.grasmanek94.saidit";
-    static final String AUDIO_MEMORY_ENABLED_KEY = "audio_memory_enabled";
-}
